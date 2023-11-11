1c6dddedb95e0abb48685ffec8f212573cc63d79
stanley-pan <s5pan@ucsd.edu>
2023-11-10 17:19:26 -0800

    edit recipe manager

--- src/main/java/RecipeManager.java
+++ src/main/java/RecipeManager.java
@@ -21,48 +21,12 @@ public class RecipeManager {
     public static final String URI = "mongodb+srv://hek007:7GVnvvaUfbPZsgnq@recipemanager.ksn9u3g.mongodb.net/?retryWrites=true&w=majority";
     public static final String CSV_FILE = "recipes.csv"; // recipe file to read from
     public static void main(String[] args) throws FileNotFoundException, IOException {
+        deleteAll();
         insertRecipes();
     }
 
     public static void insertRecipes() throws IOException{
         String line;
-        // try (MongoClient mongoClient = MongoClients.create(URI)) {
-        //     MongoDatabase recipeDB = mongoClient.getDatabase("recipes_db");
-        //     MongoCollection<Document> recipeCollections = recipeDB.getCollection("recipes");
-
-        //     FileReader fr = new FileReader(CSV_FILE); 
-        //     BufferedReader br = new BufferedReader(fr);
-        //     boolean isFirstLine = true;
-        //     while ((line = br.readLine()) != null) {
-        //         if (isFirstLine) {
-        //             isFirstLine = false;
-        //             continue; 
-        //         }
-        
-        //         int lastSemiColonIndex = line.lastIndexOf(',');
-        //         if (lastSemiColonIndex == -1) {
-        //             throw new IOException("Invalid line format: " + line);
-        //         }
-        
-        //         String name = line.substring(0, line.indexOf(','));
-        //         String description = line.substring(line.indexOf(',') + 1, lastSemiColonIndex);
-        //         String hoursString = line.substring(lastSemiColonIndex + 1);
-        //         //System.out.println(name);
-        //         //System.out.println(description);
-        //         //System.out.println(hoursString);
-        
-        //         double hours = Double.parseDouble(hoursString);
-        
-        //         Document doc = new Document("name", name)
-        //                 .append("description", description)
-        //                 .append("hours", hours);
-        //         //recipeDocuments.add(doc);
-        //         System.out.println(doc);
-        //         recipeCollections.insertOne(doc);
-        //     }
-        //     br.close();
-        //     mongoClient.close();
-        // }
         try (MongoClient mongoClient = MongoClients.create(URI)) {
             MongoDatabase recipeDB = mongoClient.getDatabase("recipes_db");
             MongoCollection<Document> recipeCollections = recipeDB.getCollection("recipes");
@@ -71,17 +35,76 @@ public class RecipeManager {
             // Convert fileReader to bufferedReader 
             BufferedReader br = new BufferedReader(fr); 
             br.readLine(); // skips first line
-            while ((line = br.readLine()) != null) { 
-                StringTokenizer st = new StringTokenizer(line, ",");
+            line = br.readLine();
+            StringTokenizer st = new StringTokenizer(line, ",");    
+            Document recipe = new Document("_id", new ObjectId());
+            recipe.append("title", st.nextToken())
+            .append("ingredients", st.nextToken())
+            .append("steps", st.nextToken());
+            System.out.println(recipe);
+            recipeCollections.insertOne(recipe);
+            // while ((line = br.readLine()) != null) { 
+            //     StringTokenizer st = new StringTokenizer(line, ",");
                 
-                Document recipe = new Document("_id", new ObjectId());
-                recipe.append("title", st.nextToken())
-                .append("ingredients", st.nextToken())
-                .append("steps", st.nextToken());
-                System.out.println(recipe);
+            //     Document recipe = new Document("_id", new ObjectId());
+            //     recipe.append("title", st.nextToken())
+            //     .append("ingredients", st.nextToken())
+            //     .append("steps", st.nextToken());
+            //     System.out.println(recipe);
             //     recipeCollections.insertOne(recipe);
-            }
+            // }
             br.close();  //closes the BR
+        }
+    }
+
+    /**
+     * Updates the entries, given the recipe name
+     */
+    public static void updateHours(Document recipe) {
+        try (MongoClient mongoClient = MongoClients.create(URI)) {
+            MongoDatabase recipeDB = mongoClient.getDatabase("recipe_db");
+            MongoCollection<Document> recipeCollections = recipeDB.getCollection("recipes");
+
+            Bson filter = eq("recipe", "Savory Spinach Delight");
+            Bson updateOperation = set("hours", 4.5);
+            UpdateResult updateResult = recipeCollections.updateOne(filter, updateOperation);
+
+            System.out.println("updating the recipe hours");
+            System.out.println(recipeCollections.find(filter).first().toJson());
+        }
+    }
+
+    /**
+     * Deletes one recipe, given a name
+     * @param recipeName recipe to delete
+     */
+    public static void deleteRecipe(ObjectId id) {
+        try (MongoClient mongoClient = MongoClients.create(URI)) {
+            MongoDatabase recipeDB = mongoClient.getDatabase("recipe_db");
+            MongoCollection<Document> recipeCollections = recipeDB.getCollection("recipes");
+            Bson filter = eq("_id", id);
+            DeleteResult result = recipeCollections.deleteOne(filter);
+            // Bson filter = eq("recipe", "Savory Spinach Delight");
+            // Bson updateOperation = set("hours", 4.5);
+            // UpdateResult updateResult = recipeCollections.updateOne(filter, updateOperation);
+
+            // System.out.println("updating the recipe hours");
+            // System.out.println(recipeCollections.find(filter).first().toJson());
+
+            // delete one document
+            System.out.println(result);
+        }
+    }
+
+    /**
+     * Helper to delete all recipe entries
+     */
+    public static void deleteAll() {
+        try (MongoClient mongoClient = MongoClients.create(URI)) {
+            MongoDatabase recipeDB = mongoClient.getDatabase("recipe_db");
+            MongoCollection<Document> recipeCollections = recipeDB.getCollection("recipes");
+
+            recipeCollections.deleteMany(new Document());
         }
     }
 }
