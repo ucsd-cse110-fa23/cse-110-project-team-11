4c0b2c45319aa1348b0c5d3f9b5dbec0cd71ae62
Jeremy Ding <jeremyding@MacBook-Pro-12.lan>
2023-11-17 04:53:28 -0800

    added fork

--- src/test/java/AppGuiTest.java
+++ src/test/java/AppGuiTest.java
@@ -14,22 +14,6 @@ import org.testfx.framework.junit5.ApplicationTest;
 import org.testfx.framework.junit5.Start;
 import static org.testfx.assertions.api.Assertions.assertThat;
 
- class AppGui extends Application {
-    public static Label message;
-
-    @Override
-    public void start(Stage stage) {
-        message = new Label("Welcome!");
-
-        stage.setScene(new Scene(new StackPane(message)));
-        stage.show();
-    }
-
-    public static Label getLabel() {
-        return message;
-    }
-}
-
 @ExtendWith(ApplicationExtension.class)
 public class AppGuiTest {
 
